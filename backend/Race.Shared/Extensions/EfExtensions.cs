c99a6b8e646780454ead97cbcb80d60ffe680ff2
Zoltan Suto <suto.zoli92@gmail.com>
2020-08-25 19:24:30 +0200

    Pagination added

---
+++ backend/Race.Shared/Extensions/EfExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Race.Shared.Extensions
+{
+    public static class EfExtensions
+    {
+        public static Task<List<TSource>> ToListAsync<TSource>(this IQueryable<TSource> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (!(source is IAsyncEnumerable<TSource>))
+                return Task.FromResult(source.ToList());
+
+            return source.ToListAsync();
+        }
+    }
+}
---
+++ backend/Race.Shared/Paging/PagedList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Race.Shared.Paging
+{
+    public class PagedList<T> : IPagedList<T>
+    {
+        private PagedList(IList<T> data, int count, int pageIndex, int pageSize)
+        {
+
+            Data = data;
+            Count = count;
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        }
+
+        /// <summary>
+        /// The data result (JSON array)
+        /// </summary>
+        public IList<T> Data { get; set; }
+        /// <summary>
+        /// Zero-based Index of Current page
+        /// </summary>
+        public int PageIndex { get; set; }
+        /// <summary>
+        /// Number of items contained in each page.
+        /// </summary>
+        public int PageSize { get; set; }
+        /// <summary>
+        /// Total items count
+        /// </summary>
+        public int Count { get;  set; }
+        /// <summary>
+        /// Total pages count
+        /// </summary>
+        public int TotalPages { get; set; }
+        /// <summary>
+        /// True if the current page has a previous page
+        /// </summary>
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 0);
+            }
+        }
+
+        /// <summary>
+        /// True if the current page has a next page
+        /// </summary>
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex + 1 < TotalPages);
+            }
+        }
+
+        public static PagedList<T> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = source.Count();
+            source = source.Skip(pageIndex * pageSize).Take(pageSize);
+            var data = source.ToList();
+
+            return new PagedList<T>(data, count, pageIndex, pageSize);
+
+        }
+    }
+}
