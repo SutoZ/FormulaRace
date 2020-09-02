d7a1028d55c885ae30164dd6696b2e6c5a9bd927
Zoltan Suto <suto.zoli92@gmail.com>
2020-09-02 20:56:03 +0200

    test initialization

---
+++ backend/Race.Tests/Scenarios/PilotControllerTest.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Race.Model.Models;
+using Race.Repo.Dtos.Pilots;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Race.Tests.Scenarios
+{
+    public class PilotControllerTest : IClassFixture<RaceWebApplicationFactory>
+    {
+        private readonly RaceWebApplicationFactory factory;
+        private readonly JsonSerializerSettings settings;
+
+        public PilotControllerTest(RaceWebApplicationFactory factory)
+        {
+            this.factory = factory;
+            this.settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore
+            };
+        }
+
+        [Fact]
+        public async void Get_Pilot_By_Id_Works()
+        {
+            //Arrange
+            var client = factory.CreateClient();
+            Pilot pilot = CreatePilot();
+
+            var expectedResult = new List<PilotDetailsDto> { new PilotDetailsDto(pilot) };
+
+            //Act
+            var response = await client.GetAsync($"api/pilot/{pilot.Id}");
+
+
+            //Assert
+            settings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var responseContent =
+                JsonConvert.DeserializeObject<List<PilotDetailsDto>>(stringResponse, settings);
+
+            responseContent.Should().BeEquivalentTo(expectedResult);
+        }
+
+        private static Pilot CreatePilot()
+        {
+            return new Pilot
+            {
+                Id = 1000,
+                Code = "Test",
+                Name = "Test Pilot",
+                Nationality = "HUN",
+                Number = "33",
+                TeamId = 1000,
+            };
+        }
+    }
+}
---
+++ backend/Race.Tests/TestStartup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Race.Repo.ApplicationContext;
+using Race.Web;
+using System;
+
+namespace Race.Tests
+{
+    public class TestStartup : Startup
+    {
+        public TestStartup(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public override void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var serviceProvirder = app.ApplicationServices;
+            var db = serviceProvirder.GetRequiredService<RaceContext>();
+
+            var logger = serviceProvirder.GetRequiredService<ILogger<RaceWebApplicationFactory>>();
+
+            db.Database.OpenConnection();
+            db.Database.EnsureCreated();
+            try
+            {
+                AddTestData(db);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+                throw;
+            }
+
+            base.Configure(app, env);
+        }
+
+        public virtual void AddTestData(RaceContext db)
+        {
+            TestDataSeed.SeedData(db);
+        }
+
+        protected override void AddDbContext(IServiceCollection services, bool isDevelopment = false)
+        {
+            services.AddDbContext<RaceContext>(options =>
+            {
+                if (isDevelopment)
+                {
+                    options.UseInMemoryDatabase(Configuration.GetConnectionString("RaceConnection"));
+                    options.EnableSensitiveDataLogging();
+                }
+            });
+        }
+    }
+}
