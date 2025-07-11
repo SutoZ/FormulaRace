d99ae80a57da55cee4873978eebcdfb1c88f669c
Zolee <suto.zoli92@hotmail.com>
2025-07-11 15:38:26 +0200

    Shared utilities added

---
+++ backend/microservices/shared/Race.Shared/Race.Shared.Utilities/Extensions/EfExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Race.Shared.Utilities.Extensions;
+
+public static class EfExtensions
+{
+    public static async Task<IReadOnlyList<TSource>> ToListSafeAsync<TSource>(this IQueryable<TSource> source, CancellationToken token)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        if (source.Provider is IAsyncQueryProvider)
+            return await EntityFrameworkQueryableExtensions.ToListAsync(source, token);
+
+        return await source.ToListAsync(token);
+    }
+}
---
+++ backend/microservices/shared/Race.Shared/Race.Shared.Utilities/Paging/IPagedList.cs
@@ -0,0 +1,16 @@
+﻿namespace Race.Shared.Utilities.Paging;
+
+public interface IPagedList<T> where T : class
+{
+    IReadOnlyList<T> Data { get; set; }
+    string FilterColumn { get; set; }
+    string FilterQuery { get; set; }
+    bool HasNextPage { get; }
+    bool HasPreviousPage { get; }
+    int PageIndex { get; set; }
+    int PageSize { get; set; }
+    string SortColumn { get; set; }
+    string SortOrder { get; set; }
+    int TotalCount { get; set; }
+    int TotalPages { get; set; }
+}
