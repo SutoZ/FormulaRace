30997065e2813057839010e0c4e2b05894054895
Zolee <suto.zoli92@hotmail.com>
2025-07-24 15:28:14 +0200

    Models update

--- frontend/src/app/race-administration/models/team.models.ts
+++ frontend/src/app/race-administration/models/team.models.ts
@@ -1,7 +1,23 @@
-export class ITeamListViewModel {
-    id!: string;
-    name!: string;
-    dateOfFoundation!: string;
-    ownerName!: string;
-    championShipPoints!: string;
+export interface ITeamListViewModel {
+  id: string;
+  name: string;
+  dateOfFoundation: string;
+  ownerName: string;
+  championShipPoints: string;
+}
+
+export class TeamListViewModel implements ITeamListViewModel {
+  id: string;
+  name: string;
+  dateOfFoundation: string;
+  ownerName: string;
+  championShipPoints: string;
+
+  constructor(init?: Partial<ITeamListViewModel>) {
+    this.id = init?.id || '';
+    this.name = init?.name || '';
+    this.dateOfFoundation = init?.dateOfFoundation || '';
+    this.ownerName = init?.ownerName || '';
+    this.championShipPoints = init?.championShipPoints || '';
+  }
 }
